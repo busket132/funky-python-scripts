928b79ce9a80ccceaf0ad15fd76a6fdd9b1beccd
bucket <155188548+busket132@users.noreply.github.com>
2025-05-29 18:18:20 +0300

    Add files via upload

---
+++ EZTkinter.py
@@ -0,0 +1,104 @@
+import tkinter as tk
+import subprocess as sp
+import os
+import sys
+
+root = tk.Tk()
+
+
+
+def get_executable_name():
+    # Получаем полный путь к исполняемому файлу
+    if getattr(sys, 'frozen', False):
+        # Если это скомпилированный exe
+        return os.path.basename(sys.executable)
+    else:
+        # Если это Python скрипт
+        return os.path.basename(sys.argv[0])
+
+def Resize_Enabler(string1):
+    if string1.lower() == "true":
+        root.resizable(True, True)
+    else:
+        root.resizable(False, False)
+
+def Add_Attributes(*args):
+    for arg in args:
+        root.attributes(arg, True)
+
+buttons = {}
+
+def Add_Button(string1,*args):
+    for arg in args:
+        buttons["button"+str(arg)] = tk.Button(root, text=string1)
+        buttons["button"+str(arg)].pack()
+    return buttons["button"+str(args[0])]
+
+labels = {}
+
+def Add_Label(string1,*args):
+    for arg in args:
+        labels["label"+str(arg)] = tk.Label(root, text=string1)
+        labels["label"+str(arg)].pack()
+    return labels["label"+str(args[0])]
+
+entries = {}
+
+def Add_Entry_DV(string1, value1="", *args):
+    var = tk.StringVar(value=value1)
+    if args:
+        for arg in args:
+            entries["entry"+str(arg)] = tk.Entry(root, textvariable=var)
+            entries["entry"+str(arg)].pack()
+        return entries["entry"+str(args[0])]
+    else:
+        entry = tk.Entry(root, textvariable=var)
+        entry.pack()
+        return entry
+
+
+
+def Boolean(value1):
+    return tk.BooleanVar(value=value1)
+
+def Double(value1=0.0):
+    return tk.DoubleVar(value=value1)
+
+def Int(value1=0):
+    return tk.IntVar(value=value1)
+
+def String(value1):
+    return tk.StringVar(value=value1)
+
+def Get(value1):
+    return value1.get
+
+def CreateWindow(value1,value2, string1):
+    executable_name = get_executable_name()
+        # Убираем расширение .exe если оно есть
+    executable_name_NoExe = os.path.splitext(executable_name)[0]
+    #script_name = os.path.basename(__file__)
+    #print(script_name)
+    root.geometry(str(value1) + "x" + str(value2))
+    root.title(string1)
+    def on_closing():
+        sp.Popen('taskkill /im ' + str(executable_name) + ' /f')
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    root.mainloop()
+
+
+
+#CreateWindow()
+
+    
+
+
+#CreateWindow(100,100)
+
+'''def on_closing():
+    
+    # Terminate minecraft.exe process
+    sp.Popen('taskkill /im python.exe /f')
+    root.destroy()
+
+root.protocol("WM_DELETE_WINDOW", on_closing)'''
